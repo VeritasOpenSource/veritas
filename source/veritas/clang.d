ea7ac7da0aeaf2cd43204f5975be572490467891
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-01-01 17:59:13 +0700

    Added some entities and prototypes for clang

--- source/veritas/clang.d
+++ source/veritas/clang.d
@@ -6,14 +6,35 @@ extern(C) {
     alias CXIndex = void*;
     alias CXTranslationUnit = void*;
     alias CXClientData = void*;
-    alias CXSourceLocation = void*;
+    alias CXFile = void*;
+
+    struct CXString { 
+        const(char)* data; 
+        void* private_flags; 
     
-    struct CXString { const(char)* data; void* private_flags; }
-    struct CXCursor { int kind; int xdata; void*[3] data; }
+    }
+
+    struct CXCursor { 
+        int kind; 
+        int xdata; 
+        void*[3] data; 
+    }
+
+    struct CXSourceLocation { 
+        void*[2] data; 
+        uint int_data; 
+    }
+
+    struct CXSourceRange {
+        void*[2] 	ptr_data;
+        uint 	begin_int_data;
+        uint 	end_int_data;
+    }
     
     alias CXCursorVisitor = uint function(CXCursor, CXCursor, CXClientData);
     
     CXIndex clang_createIndex(int, int);
+    CXString clang_getFileName(CXFile);
     void clang_disposeIndex(CXIndex);
     CXTranslationUnit clang_createTranslationUnitFromSourceFile(CXIndex, const(char)*, int, const(char)**, uint, void*);
     CXTranslationUnit clang_parseTranslationUnit(
@@ -27,8 +48,16 @@ extern(C) {
     CXString clang_getCursorSpelling(CXCursor);
     const(char)* clang_getCString(CXString);
     void clang_disposeString(CXString);
+    CXSourceRange clang_getCursorExtent(CXCursor);
+    uint clang_isCursorDefinition(CXCursor);
+    void clang_getSpellingLocation(CXSourceLocation, CXFile, uint*, uint*, uint*);
+    void clang_getFileLocation(CXSourceLocation, CXFile, uint*, uint*, uint*);
+    CXSourceLocation clang_getRangeStart(CXSourceRange);
+    CXSourceLocation clang_getRangeEnd(CXSourceRange);
+    void clang_getExpansionLocation(CXSourceLocation, void*, uint* start, uint* start_col, void*);
     uint clang_visitChildren(CXCursor, CXCursorVisitor, CXClientData);
     int clang_Location_isInSystemHeader(CXSourceLocation location);
+    CXSourceLocation clang_getCursorLocation(CXCursor cursor);
     CXSourceLocation clang_getCursorLocation(CXCursor cursor);
     CXCursor clang_getCursorReferenced(CXCursor cursor);
 }
