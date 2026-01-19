37b8f9cbb187a726743c60ce7ae7c80a7f8f884b
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-01-19 23:58:21 +0700

    First IPC sketch

---
+++ source/veritas/ecosystem/events.d
@@ -0,0 +1,34 @@
+module veritas.ecosystem.events;
+
+enum CommandType {
+    AddProject
+}
+
+interface VrtsCommand {
+    CommandType getType();
+}
+
+class ComAddProject : VrtsCommand {
+    string path;
+
+    override CommandType getType() {
+        return CommandType.AddProject;
+    }
+}
+
+enum EventType {
+    ProjectAdded
+}
+
+interface VrtsEvent {
+    EventType getType();
+}
+
+class EventProjectAdded : VrtsEvent {
+    string path;
+
+    override EventType getType() {
+        return EventType.ProjectAdded;
+    } 
+}
+
