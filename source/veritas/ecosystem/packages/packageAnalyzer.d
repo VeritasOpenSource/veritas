d869687bd00c428f369e177d763b404269fa2a0c
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-03-07 20:51:51 +0700

    Cleaned packageAnalyzer

--- source/veritas/ecosystem/packages/packageAnalyzer.d
+++ source/veritas/ecosystem/packages/packageAnalyzer.d
@@ -1,33 +1,22 @@
 module veritas.ecosystem.packages.packageAnalyzer;
 
-import std.string;
-import std.path;
-
-import veritas.ipc;
-import veritas.clang;
-import veritas.ecosystem;
-import veritas.common.collector;
-import veritas.common.dataStorage;
-import veritas.ecosystem.functions;
-import veritas.ecosystem.calls;
-import veritas.ecosystem.sourceFiles;
-import veritas.common.toolkit;
-import veritas.clang;
+import veritas.ecosystem.ecosystem;
 import veritas.ecosystem.packages;
+import veritas.common.collector;
 import veritas.common.analyzer;
 
 
-class VrtsPackageAnalyzer {
+class VrtsPackageAnalyzer : VrtsAnalyzer {
 
 	VrtsPackagesCollector	collector;
 
 	ref auto packages() inout @property {
-		return collector.storage.data;
+		return collector.storage;
 	}
 
 	this(VrtsEcosystem ecosystem) {
         //No need ecosystem data here
-		collector = new VrtsPackagesCollector(null);
+		collector = new VrtsPackagesCollector();
 	}
 
     void addPackage(string metadataPath) {
