8672198260ba01414fdd1d7f87ae4b69243b36fc
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-03-07 20:39:27 +0700

    Cleaned veritas.common.toolkit

--- source/veritas/common/toolkit.d
+++ source/veritas/common/toolkit.d
@@ -1,12 +1,11 @@
 module veritas.common.toolkit;
 
-import veritas.ecosystem.sourceFiles;
-import veritas.ecosystem.functions;
-import veritas.ecosystem.calls;
-// import veritas.callsCollector;
-// import veritas.functionsCollector;
-import veritas.ecosystem.packages;
-
+import veritas.ecosystem.sourceFiles : VrtsSourceFile;
+import veritas.ecosystem.functions :
+	VrtsFunctionsAnalyzer,
+	VrtsFunction;
+import veritas.ecosystem.calls : VrtsCallsAnalyzer;
+import veritas.ecosystem.packages : VrtsPackage;
 
 abstract class VrtsToolkit {
 	abstract void extractFunctionsFromSourceFile(VrtsFunctionsAnalyzer funcsAnalyzer, VrtsSourceFile sourceFile);
