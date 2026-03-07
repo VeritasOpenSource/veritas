182f56a9f4518d999dd075d04571b8e0eca7736d
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-03-07 21:21:03 +0700

    Cleaned callsCollector module

--- source/veritas/ecosystem/calls/callsCollector.d
+++ source/veritas/ecosystem/calls/callsCollector.d
@@ -1,28 +1,16 @@
 module veritas.ecosystem.calls.callsCollector;
 
-// module veritas.ecosystem.sourceAnalyzer;
-
-import veritas.ecosystem;
-import veritas.ecosystem.sourceFiles;
-import std.stdio;
 import std.file;
 import std.array;
-
 import std.algorithm;
 import std.conv;
 
+import veritas.ecosystem.calls.call;
+import veritas.ecosystem.ecosystem;
 import veritas.common.collector;
-import veritas.ecosystem.packages;
-import veritas.ipc.events;
-import veritas.ecosystem.calls;
 import veritas.ecosystem.functions;
-import std.array;
 
 class VrtsCallsCollector : VrtsCollector!VrtsFunctionCall {
-    VrtsEventBus eventBus;
-    // VrtsEcosystem ecosystem;
-    // VrtsSourceCollector sourceCollector;
-
     VrtsFunctionsCollector functionsCollector;
     struct FunctionCalls {
 		VrtsFunctionCall[] ongoing;
@@ -30,24 +18,11 @@ class VrtsCallsCollector : VrtsCollector!VrtsFunctionCall {
 	}
     FunctionCalls[VrtsFunction] callsPerFunctions;
 
-    // struct FunctionSourceAssoc {
-    //     VrtsPackage pkg;
-    //     VrtsSourceFile[] file;
-    // }
-
-    // FunctionSourceAssoc[] associatedFunctions;
-
-    void setEventsBus(VrtsEventBus eventBus) {
-        this.eventBus = eventBus;
-    }
-
     uint getNewId() {
         return cast(uint)storage.length;
     }
 
     this(	VrtsEcosystem ecosystem) {
-        // this.ecosystem = ecosystem;
-		// this.sourceCollector = collector; 
         this.functionsCollector = ecosystem.functionsCollector;
     }
 
@@ -56,21 +31,13 @@ class VrtsCallsCollector : VrtsCollector!VrtsFunctionCall {
     }
 
     VrtsFunctionCall[] getOutgoingCalls(VrtsFunction func) {
-        // writeln(func.name);
         if(func !in callsPerFunctions)
             return [];
 
         return callsPerFunctions[func].outgoing;
-        // try {
-        // }
-        // catch(Exception e) {
-        //     writeln(func.name);
-        //     return [];
-        // }
     }
 
     auto getFunctionsWithoutCalls() {
         return callsPerFunctions.byKeyValue().filter!(a => a.value.outgoing.length == 0);
-        // return functionsCollector.storage.data.filter!((a) => a.calls.length == 0);
     }
 }
