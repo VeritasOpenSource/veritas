62287469b25b8c71ee30a0463175d3659f20f1ef
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-03-07 21:14:53 +0700

    Cleaned functionsCollector module

--- source/veritas/ecosystem/functions/functionsCollector.d
+++ source/veritas/ecosystem/functions/functionsCollector.d
@@ -1,39 +1,25 @@
 module veritas.ecosystem.functions.functionsCollector;
 
-import veritas.ecosystem;
-import veritas.ecosystem.sourceFiles;
-import std.stdio;
+// import std.stdio;
 import std.file;
-import std.array;
-
-import std.algorithm;
-import std.conv;
+// import std.array;
+// import std.algorithm;
+// import std.conv;
 import std.path;
 
+import veritas.ecosystem.functions;
+import veritas.ecosystem.sourceFiles;
 import veritas.common.collector;
-import veritas.ecosystem.packages;
-import veritas.ipc.events;
+// import veritas.ecosystem.packages;
+// import veritas.ipc.events;
 
 class VrtsFunctionsCollector : VrtsCollector!VrtsFunction {
-    VrtsEventBus eventBus;
-    // VrtsEcosystem ecosystem;
 
-    // VrtsSourceCollector sourceCollector;
     VrtsFunction[][VrtsSourceFile]    functionsPerFile;
-
-    void setEventsBus(VrtsEventBus eventBus) {
-        this.eventBus = eventBus;
-    }
-
-    // this(VrtsEcosystem ecosystem)
 
     uint getNewId() {
         return cast(uint)storage.length;
     }
-
-    // void processFunctionsFromSourceFiles(VrtsFunction[] funcs) {
-    //     funcs.each!(a => this.addFunction(a));
-    // }
 
     bool checkFunctionEdentity(VrtsFunction func1, VrtsSourceFile sourceFile2, string name) {
         auto pathFile = func1.file.getPath.dirName;
