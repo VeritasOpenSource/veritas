438405bff58100f4140351c0f70032712e0c05fe
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-03-07 21:17:58 +0700

    Cleaned functionsCollector

--- source/veritas/ecosystem/functions/functionsAnalyzer.d
+++ source/veritas/ecosystem/functions/functionsAnalyzer.d
@@ -1,33 +1,18 @@
 module veritas.ecosystem.functions.functionsAnalyzer;
 
-import std.string;
-import std.path;
 import std.algorithm;
 import std.range;
 
-import veritas.ipc;
-import veritas.clang;
-import veritas.ecosystem;
-import veritas.common.collector;
-import veritas.common.dataStorage;
-import veritas.ecosystem.functions;
-import veritas.ecosystem.calls;
-import veritas.ecosystem.sourceFiles;
-import veritas.common.toolkit;
-import veritas.clang;
-import veritas.ecosystem.packages;
 import veritas.common.analyzer;
+import veritas.ecosystem.ecosystem;
+import veritas.ecosystem.functions;
+import veritas.ecosystem.sourceFiles;
 
 class VrtsFunctionsAnalyzer : VrtsAnalyzer {
 
 	VrtsFunctionsCollector	collector;
 
-	// ref auto packages() inout @property {
-	// 	return collector.storage.data;
-	// }
-
 	this(VrtsEcosystem ecosystem) {
-        //No need ecosystem data here
 		collector = new VrtsFunctionsCollector();
 	}
 
