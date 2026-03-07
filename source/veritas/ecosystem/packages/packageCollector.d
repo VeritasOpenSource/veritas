2875af02709c7ce37d0ff2649f033eee3db15ce4
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-03-07 20:51:36 +0700

    Cleaned packageCollector

--- source/veritas/ecosystem/packages/packageCollector.d
+++ source/veritas/ecosystem/packages/packageCollector.d
@@ -2,16 +2,8 @@ module veritas.ecosystem.packages.packageCollector;
 
 import veritas.ecosystem.packages.pkg;
 import veritas.common.collector;
-import veritas.ecosystem;
 
 class VrtsPackagesCollector : VrtsCollector!VrtsPackage {
-	VrtsEcosystem ecosystem;
-
-	this(VrtsEcosystem ecosystem) {
-		super();
-		this.ecosystem = ecosystem;
-	}
-
 	void addPackage(VrtsPackage pkg) {
         storage.add(pkg);
 	}
