d66ec72f832cedb629b9531b7d6bdad8f8a5a228
Roman Vlasov <vlasovroman.ru@yandex.ru>
2026-03-08 09:32:56 +0700

    Fix import in rings module

--- source/veritas/ecosystem/rings/package.d
+++ source/veritas/ecosystem/rings/package.d
@@ -2,5 +2,6 @@ module veritas.ecosystem.rings;
 
 public {
 	import veritas.ecosystem.rings.ring;
-	import veritas.ecosystem.rings.ringsCollector;	
+	import veritas.ecosystem.rings.ringsCollector;
+	import veritas.ecosystem.rings.ringsAnalyzer;
 }
